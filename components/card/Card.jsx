16f3e50b9bf59c64b8ebcaea95a670a7bf440d77
Rayhan <rayhansatriabudi@gmail.com>
2024-09-05 20:18:58 +0700

    menambah background card di setiap icon skill

--- components/card/Card.jsx
+++ components/card/Card.jsx
@@ -13,6 +13,9 @@ const Card = () => {
   const progress_html = 80;
   const progress_css = 70;
   const progress_js = 45;
+  const progress_py = 75;
+  const progress_react = 40;
+  const progress_tailwind = 10;
   
   return (
     <div className='Card-container'>
@@ -36,42 +39,62 @@ const Card = () => {
           <div className='column1'>
 
             <div className='html-bar'>
-                <Typography variant="h6" gutterBottom sx={{position: 'relative', left: '150px', color: 'white', top: '50px', fontFamily: 'nasa'}}>
+                <Typography variant="h6" gutterBottom sx={{position: 'relative', left: '170px', color: 'white', top: '50px', fontFamily: 'nasa', width: '100px'}}>
                   80%
                 </Typography>
               <div className='spinner spin1'><AiOutlineHtml5 className='icon html-icon'/></div>
               <Box sx={{width: '100%'}}>
-                <LinearProgress variant="determinate" value={progress_html} className='html-bar' 
-                sx={{width: '500px', minHeight: '10px', position: 'relative', left: '130px', bottom: '10px'}} color='secondary'/>
+                <LinearProgress variant="determinate" value={progress_html} 
+                sx={{width: '500px', minHeight: '10px', position: 'relative', left: '170px', bottom: '10px', borderRadius: '20px'}} color='secondary'/>
               </Box>
             </div>
 
             <div className='css-bar'>
-              <Typography variant='h6' gutterBottom sx={{position: 'relative', left: '150px', color: 'white', top: '50px', fontFamily: 'nasa'}}>60%</Typography>
+              <Typography variant='h6' gutterBottom sx={{position: 'relative', left: '170px', color: 'white', top: '50px', fontFamily: 'nasa', width: '100px'}}>60%</Typography>
               <div className='spinner spin1'><FaCss3 className='icon css-icon'/></div>
               <Box sx={{width: '100%'}}>
                 <LinearProgress variant='determinate' value={progress_css} className='css-bar' color='secondary' 
-                sx={{width: '500px', minHeight: '10px', position: 'relative', left: '130px', bottom: '10px'}}/>
+                sx={{width: '500px', minHeight: '10px', position: 'relative', left: '170px', bottom: '10px', borderRadius: '20px'}}/>
               </Box>
             </div>
 
             <div className="js-bar">
-                <Typography variant='h6' gutterBottom sx={{position: 'relative', left: '150px', color: 'white', top: '50px', fontFamily: 'nasa'}}>45%</Typography>
-                <div className='spinner spin2'><IoLogoJavascript className='icon js-icon'/></div>
+                <Typography variant='h6' gutterBottom sx={{position: 'relative', left: '170px', color: 'white', top: '50px', fontFamily: 'nasa', width: '100px'}}>45%</Typography>
+                <div className='spinner spin1'><IoLogoJavascript className='icon js-icon'/></div>
                 <Box sx={{width: '100%'}}>
                   <LinearProgress variant='determinate' value={progress_js} classname='js-bar' color='secondary'
-                  sx={{width: '500px', minHeight: '10px', position: 'relative', left: '130px', bottom: '10px'}}/>
+                  sx={{width: '500px', minHeight: '10px', position: 'relative', left: '170px', bottom: '10px', borderRadius: '20px'}}/>
                 </Box>
             </div>
 
           </div>
 
-
-
           <div className='column2'>
-            <div className='spinner spin3'><AiOutlinePython className='icon py-icon'/></div>
-            <div className='spinner spin4'><FaReact className='icon react-icon'/></div>
-            <div className='spinner spin5'><RiTailwindCssFill className='icon tailwind-icon'/></div>
+            <div className='py-bar'>
+              <Typography variant='h6' gutterBottom sx={{position: 'relative', left: '170px', color: 'white', top: '50px', fontFamily: 'nasa', width: '100px'}}>75%</Typography>
+              <div className='spinner spin1'><AiOutlinePython className='icon py-icon'/></div>
+              <Box sx={{width: '100%'}}>
+                <LinearProgress variant='determinate' value={progress_py} color='secondary'
+                sx={{width: '500px', minHeight: '10px', position: 'relative', left: '170px', bottom: '10px', borderRadius: '20px'}}/>
+              </Box>
+            </div>
+           
+            <div className='react-bar'>
+              <Typography variant='h6' gutterBottom sx={{position: 'relative', left: '170px', color: 'white', top: '50px', fontFamily: 'nasa', width: '100px'}}>40%</Typography>
+              <div className='spinner spin1'><FaReact className='icon react-icon'/></div>
+              <Box sx={{width: '100%'}}>
+                <LinearProgress variant='determinate' value={progress_react} color='secondary' 
+                sx={{width: '500px', minHeight: '10px', position: 'relative', left: '170px', bottom: '10px', borderRadius: '20px'}}/>
+              </Box>
+            </div>
+            
+            <div className='tailwind-bar'>
+            <Typography variant='h6' gutterBottom sx={{position: 'relative', left: '170px', color: 'white', top: '50px', fontFamily: 'nasa', width: '100px'}}>10%</Typography>
+              <div className='spinner spin1'><RiTailwindCssFill className='icon tailwind-icon'/></div>
+               <Box sx={{width: '100%'}}>
+                 <LinearProgress variant='determinate' value={progress_tailwind} color='secondary' sx={{width: '500px', minHeight: '10px', position: 'relative', left: '170px', bottom: '10px', borderRadius: '20px'}}/>
+               </Box>
+            </div>
           </div>
         </div>
       </div>
